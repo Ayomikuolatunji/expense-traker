d57913824f8e0ac852efb1e4a6f519bd8bd6b450
ayomikuolatunji <ayomikuolatunji@gmail.com>
2022-02-24 02:27:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Switch,Route} from "react-router-dom"
+
+
+
+export default function App(){
+    return(
+        <div className="text-center">
+            <h1>
+              App
+            </h1>
+        </div>
+    )
+}
