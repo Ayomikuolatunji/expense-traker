061792b68f6c9203937acc1748d30a0e2cc8cc03
ayomikuolatunji <ayomikuolatunji@gmail.com>
2022-02-24 21:17:01 +0100

    routing pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 // import {Switch,Route} from "react-router-dom"
 import Header from "./components/Header/Header"
 import Home from "./Pages/Home"
+import {Route,Switch} from "react-router-dom"
 
 export default function App(){
     return(
         <div className="text-center">
             <Header/>
+            <Switch>
+                <Route path="/" component={Home}/>
+            </Switch>
             <Home/>
         </div>
     )
